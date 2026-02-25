c4315a136465557ab7abdc2f5d53f551ca775734
Dheeraj <dheerajdhiraj91@gmail.com>
2026-02-25 17:32:28 +0530

    Fixed firebase config syntax error

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -3,9 +3,9 @@ const firebaseConfig = {
     apiKey: "AIzaSyA3y3QLa89nJGrhhz7EdY9uXHrwG7qXX_s",
     authDomain: "quize-platform-b5154.firebaseapp.com",
     projectId: "quize-platform-b5154",
-    storageBucket: "quize-platform-b5154.firebasestorage.app",
+    storageBucket: "quize-platform-b5154.appspot.com",
     messagingSenderId: "93074908972",
-    appId: "1:93074908972:web:4050c53757e5f62011ee89"
+    appId: "1:93074908972:web:4050c53757e5f62011ee89",
     measurementId: "G-Q1MKKV83QR"
 };
 
